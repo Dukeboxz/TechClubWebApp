e77d18ff0c21a2e905bf7fb20812e94d654bb527
Dukeboxz <u04sjj@gmail.com>
2019-06-25 21:43:15 +0100

    Fix problems with express loading

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
+const badgeRouter = require('./routers/badges')
 
-const app = express
+const app = express()
 
 app.use(express.json())
 app.use(userRouter)
